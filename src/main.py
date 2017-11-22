9d1cc9f456b8e82b99f758dad437e82a6ced24b2
Raphael Jacob <r.jacob2002@gmail.com>
2017-11-22 17:11:10 +0100

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+import os
+
+
+class KDEInstance:
+    def execute(self, cmd, options=[], data=''):
+        optionsData = []
+        for o in options:
+            if type(o) == tuple:
+                a, b = o
+                optionsData.append(a)
+                optionsData.append(b)
+            else:
+                optionsData.append(o)
+        callList = ['loginctl', ' '.join(optionsData), cmd, data]
+        callList = [x for x in callList if x != '']
+        call = ' '.join(callList)
+        out = os.popen(call).read().splitlines()
+        return(out)
+
+    def readTable(self, rawData):
+        tableData = []
+        for rawLine in rawData:
+            line = []
+            for word in rawLine.split(' '):
+                if word == '':
+                    continue
+                line.append(word)
+            if line != []:
+                tableData.append(line)
+        tableData.pop()
+        data = []
+        header = tableData[0]
+        for line in tableData[1:]:
+            assert(len(line) <= len(header))
+            lineData = {}
+            for i in range(len(line)):
+                lineData[header[i]] = line[i]
+            data.append(lineData)
+        return(data)
+
+    def listSessions(self):
+        return(self.readTable(self.execute('list-sessions')))
+
+    def listUsers(self):
+        return(self.readTable(self.execute('list-users')))
+
+    def lockSession(self, sid):
+        self.execute('lock-session', data=str(sid))
+
+    def unlockSession(self, sid):
+        self.execute('unlock-session', data=str(sid))
+
+    def findUserSessions(self, name):
+        data = self.listSessions()
+        sessions = []
+        for session in data:
+            if session['USER'] == name:
+                sessions.append(int(session['SESSION']))
+        return(sessions)
+
+    def userLockAllSessions(self, name):
+        sessions = self.findUserSessions(name)
+        for session in sessions:
+            self.lockSession(session)
+
+    def userUnlockAllSessions(self, name):
+        sessions = self.findUserSessions(name)
+        for session in sessions:
+            self.unlockSession(session)
+
+
+KDE = KDEInstance()
